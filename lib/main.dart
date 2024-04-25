83b5c979919c85cf88c093e228d189e52e40b69a
faseehcbt <117721811+faseehtkcbt@users.noreply.github.com>
2024-04-25 15:10:38 +0530

    changes -- debug shower remove

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: Constants.appName,
       theme: AppTheme.lightTheme,
       routes: Routes.route,
