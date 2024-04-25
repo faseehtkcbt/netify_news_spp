454cd7ab8bec782fd5b24e995356fec2eae23faf
faseehcbt <117721811+faseehtkcbt@users.noreply.github.com>
2024-04-25 16:31:04 +0530

    changes -- nothing change remove

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //
       debugShowCheckedModeBanner: false,
       title: Constants.appName,
       theme: AppTheme.lightTheme,
